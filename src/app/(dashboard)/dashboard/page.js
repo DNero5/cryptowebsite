e681ce46dc2a11cdb72b00c18bf2731d75a266ff
adelekecode <femiadeleke601@gmail.com>
2025-05-21 18:39:52 +0100

    balance

--- src/app/(dashboard)/dashboard/page.js
+++ src/app/(dashboard)/dashboard/page.js
@@ -197,7 +197,7 @@ export default function DashboardPage() {
 
         <div className="grid grid-cols-2 gap-4">
           <div className="bg-gray-900 p-4 rounded">
-            <h2 className="text-lg mb-2">💼Payment Wallet</h2>
+            <h2 className="text-lg mb-2">💼Balance</h2>
             <p className="text-2xl font-semibold">${data.view.balance}</p>
           </div>
           <div className="bg-gray-900 p-4 rounded">
